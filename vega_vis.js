952e9a0538ada9a0fbaa10ff551402704c704fe5
jpha0018 <71078991+jpha0018@users.noreply.github.com>
2021-10-10 20:53:20 +1100

    Add files via upload

--- vega_vis.js
+++ vega_vis.js
@@ -1,4 +1,9 @@
 var vg_1 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/map_vis.json";
+var vg_2 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/plastic_bar_vis.json"
 vegaEmbed("#map", vg_1).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
    
 }).catch(console.error);
