ab5a7a07bbac5de9ac2cf465095303ac400821bc
jpha0018 <71078991+jpha0018@users.noreply.github.com>
2021-10-10 17:43:36 +1100

    Update vega_vis.js

--- vega_vis.js
+++ vega_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/map1_vis.json";
+var vg_1 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/map_vis.json";
 vegaEmbed("#map", vg_1).then(function(result) {
    
 }).catch(console.error);
