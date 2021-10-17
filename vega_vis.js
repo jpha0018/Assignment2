021be5e17fb8e39e715eab2127621fb5f7b5a833
jpha0018 <71078991+jpha0018@users.noreply.github.com>
2021-10-18 02:30:23 +1100

    Add files via upload

--- vega_vis.js
+++ vega_vis.js
@@ -1,15 +1,40 @@
 var vg_1 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/map_vis.json";
 var vg_2 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/plastic_bar_vis.json"
-var vg_3 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/line_vis.json"
+var vg_3 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/bubble_vis.json"
+var vg_4 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/oil_spill.json"
+var vg_5 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/micro_plastic.json"
+var vg_6 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/marine_source.json"
+var vg_7 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/illness_pie.json"
+var vg_8 = "https://raw.githubusercontent.com/jpha0018/Assignment2/main/bar_degrade.json"
 
 vegaEmbed("#map", vg_1).then(function(result) {
    
 }).catch(console.error);
 
-vegaEmbed("#bar_chart", vg_2).then(function(result) {
+vegaEmbed("#top10_bar_chart", vg_2).then(function(result) {
    
 }).catch(console.error);
 
-vegaEmbed("#line_chart", vg_3).then(function(result) {
+vegaEmbed("#bubble_chart", vg_3).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#oil_chart", vg_4).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#micro_chart", vg_5).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#marinesource_chart", vg_6).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#illness_chart", vg_7).then(function(result) {
+   
+}).catch(console.error);
+
+vegaEmbed("#degrade_chart", vg_8).then(function(result) {
    
 }).catch(console.error);
